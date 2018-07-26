80a7a5960d54643e155f1ec5c7bf7c4cd8f41b77
Will Winder <wwinder@traackr.com>
2018-07-26 08:50:50 -0400

    Initial commit.

---
+++ src/main/java/com/willwinder/cuscribe/App.java
@@ -0,0 +1,13 @@
+package com.willwinder.cuscribe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
